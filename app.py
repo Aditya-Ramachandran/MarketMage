6e6cb15e3809ef81a457d16a7a37a63c0a8b6981
Aditya Ramachandran <aditya.199303083@muj.manipal.edu>
2023-08-22 13:14:34 +0530

    Add: Sales Analysis category code to fetch data from server

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from data_retriever import DB
 
+
 db = DB()
+
 
 st.set_page_config(page_title='MarketMage',layout='wide')
 
@@ -15,4 +17,11 @@ user_option = st.sidebar.selectbox('Choose One', ('About', 'Analytics'))
 if user_option == 'About':
     st.write('About')
 else:
-    st.write('Analytics')
+    analysis_option = st.sidebar.radio('Choose Type of Analysis', ('Sales Analysis', 'Profit and Discount Analysis', 'Segment and Mode Analysis','Yearly Analysis'))
+
+    if analysis_option == 'Sales Analysis':
+        db.get_ship_mode_avg_sales()
+        db.get_category_subcategory_profit()
+        db.get_city_state_total_sales()
+        db.get_region_category_avg_sales()
+        db.get_avg_sales_per_quantity_segment()
--- data_retriever.py
+++ data_retriever.py
@@ -4,14 +4,76 @@ import streamlit as st
 
 class DB:
     def __init__(self):
-    # connecting to the server
-        try: 
+        try:
             self.conn = mysql.connector.connect(
-                host = '127.0.0.1', 
-                user = "root",
-                password =""
+                host='127.0.0.1',
+                user="root",
+                password=""
             )
             self.mycursor = self.conn.cursor()
             print('Connection Established!')
-        except: 
+        except:
             print('Connection Failed')
+
+    def get_ship_mode_avg_sales(self):
+        self.mycursor.execute("USE store")
+        self.mycursor.execute("""
+        SELECT `Ship Mode` ,AVG(Sales) FROM store_data
+        GROUP BY `Ship Mode`
+        ORDER BY AVG(Sales) DESC;
+    """)
+        data = self.mycursor.fetchall()
+        data = pd.DataFrame(data)
+        data.rename(columns={0:'Shipment',1:'Avg Sales'}, inplace=True)
+        st.dataframe(data)
+    
+    def get_category_subcategory_profit(self):
+        self.mycursor.execute("USE store")
+        self.mycursor.execute("""
+            SELECT Category, `Sub-Category`, MAX(Profit) AS MaxProfit
+            FROM store_data
+            GROUP BY Category, `Sub-Category`
+            ORDER BY MaxProfit DESC;
+        """)
+        data = self.mycursor.fetchall()
+        data = pd.DataFrame(data)
+        data.rename(columns={0:'Category', 1:'Sub-Category', 2:'Profit'}, inplace=True)
+        st.dataframe(data)
+    
+    def get_city_state_total_sales(self):
+        self.mycursor.execute("USE store")
+        self.mycursor.execute("""
+            SELECT City, State, ROUND(SUM(Sales),2) 
+            FROM store_data
+            GROUP BY City, State
+            ORDER BY SUM(Sales) DESC;
+        """)
+        data = self.mycursor.fetchall()
+        data = pd.DataFrame(data)
+        data.rename(columns={0:'City', 1:'State', 2:'Total Sales'},inplace=True)
+        st.dataframe(data)
+    
+    def get_region_category_avg_sales(self):
+        self.mycursor.execute("USE store")
+        self.mycursor.execute("""
+            SELECT Region, Category, AVG(Sales)
+            FROM store_data
+            GROUP BY Region, Category
+            ORDER BY AVG(Sales) DESC;
+        """)
+        data = self.mycursor.fetchall()
+        data = pd.DataFrame(data)
+        data = data.rename(columns={0:'Region', 1:'Category', 2:'Avg Sales'})
+        st.dataframe(data)
+    
+    def get_avg_sales_per_quantity_segment(self):
+        self.mycursor.execute("USE store")
+        self.mycursor.execute("""
+            SELECT Segment, AVG(Sales / Quantity)
+            FROM store_data
+            GROUP BY Segment;
+        """)
+        data = self.mycursor.fetchall()
+        data = pd.DataFrame(data)
+        data = data.rename(columns={0:'Segment', 1:'Avg Sales'})
+        st.dataframe(data)
